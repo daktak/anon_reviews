f608fbc93d37a97b156d487629fab2b33c6e46d4
daktak <daktak@domo.ddns.net>
2026-04-20 16:12:34 +1000

    index.php update to feed style

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ function pageUrl($page, $tag) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Reviews Feed</title>
+    <title>Reviews</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
